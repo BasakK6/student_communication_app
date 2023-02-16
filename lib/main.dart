73da194d565ef0d3225a8e96877d106e4b2c5824
BasakK6 <katukbasak@gmail.com>
2023-02-16 17:39:45 +0300

    feature: used firebase storage to host user profile picture and showed the picture in ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:student_communication_app/firebase_utilities/google_sign_in.dart';
 import 'package:student_communication_app/pages/messages_page.dart';
 import 'package:student_communication_app/repository/messages_repository.dart';
@@ -26,7 +31,7 @@ class StudentApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),//const MainPage(title: 'Main Page'),
+      home: const SplashScreen(), //const MainPage(title: 'Main Page'),
     );
   }
 }
@@ -38,30 +43,27 @@ class MainPage extends ConsumerWidget {
 
   void _goToStudentsPage(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) =>
-          const StudentsPage(),
+      builder: (context) => const StudentsPage(),
     ));
   }
 
   void _goToTeachersPage(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) =>
-          const TeachersPage(),
+      builder: (context) => const TeachersPage(),
     ));
   }
 
   Future<void> _goToMessagesPage(BuildContext context) async {
     await Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) =>
-         const MessagesPage(),
+      builder: (context) => const MessagesPage(),
     ));
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final studentsRepository= ref.watch(studentsProvider);
+    final studentsRepository = ref.watch(studentsProvider);
     final newMessageCount = ref.watch(newMessageCountProvider);
-    final teachersRepository= ref.watch(teachersProvider);
+    final teachersRepository = ref.watch(teachersProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -75,12 +77,7 @@ class MainPage extends ConsumerWidget {
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(FirebaseAuth.instance.currentUser?.displayName ?? "No User Name"),
-            ),
+            const UserDrawerHeader(),
             ListTile(
               title: const Text('Students'),
               onTap: () {
@@ -106,11 +103,9 @@ class MainPage extends ConsumerWidget {
               title: const Text('Log out'),
               onTap: () async {
                 await signOutWithGoogle();
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => const SplashScreen(),
-                  )
-                );
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (BuildContext context) => const SplashScreen(),
+                ));
               },
             ),
           ],
@@ -147,6 +142,108 @@ class MainPage extends ConsumerWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class UserDrawerHeader extends StatefulWidget {
+  const UserDrawerHeader({
+    super.key,
+  });
+
+  @override
+  State<UserDrawerHeader> createState() => _UserDrawerHeaderState();
+}
+
+class _UserDrawerHeaderState extends State<UserDrawerHeader> {
+  late Future<Uint8List?> _profilePic;
+  late final String? uid;
+
+  String get userPicPath => "$uid.jpg";
+
+  @override
+  void initState() {
+    super.initState();
+    uid = FirebaseAuth.instance.currentUser?.uid;
+    _profilePic = _downloadProfilerPicture();
+  }
+
+  Future<Uint8List?> _downloadProfilerPicture() async {
+    final DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+        await FirebaseFirestore.instance.collection("users").doc(uid).get();
+    Map<String, dynamic>? data = documentSnapshot.data();
+
+    if (data?.keys.contains("profilePicturePath") ?? false) {
+      String pathRef = data!["profilePicturePath"];
+      Uint8List? uInt8List =
+          await FirebaseStorage.instance.ref(pathRef).getData();
+      return uInt8List; //return picture data
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+              FirebaseAuth.instance.currentUser?.displayName ?? "No User Name"),
+          const SizedBox(
+            height: 8,
+          ),
+          Stack(
+            alignment: AlignmentDirectional.topEnd,
+            children: [
+              FutureBuilder<Uint8List?>(
+                  future: _profilePic,
+                  builder: (context, snapshot) {
+                    return snapshot.hasData && snapshot.data != null
+                        ? CircleAvatar(
+                            radius: 40,
+                            backgroundImage: MemoryImage(snapshot.data!),
+                          )
+                        : const CircleAvatar(
+                            radius: 40,
+                            child: Text("BK"),
+                          );
+                  }),
+              Positioned(
+                top: -10,
+                right: -15,
+                child: IconButton(
+                  onPressed: () async {
+                    XFile? xFile = await ImagePicker()
+                        .pickImage(source: ImageSource.camera);
+                    if (xFile == null) return;
+                    final imagePath = xFile.path;
+                    final Reference profilePicturePath = FirebaseStorage
+                        .instance
+                        .ref("profile_pictures")
+                        .child(userPicPath);
+                    await profilePicturePath.putFile(File(imagePath));
+                    await FirebaseFirestore.instance
+                        .collection("users")
+                        .doc(uid)
+                        .update({
+                      "profilePicturePath": profilePicturePath.fullPath,
+                    });
+                    //data is put in the storage, now we must refresh the circle avatar
+                    setState(() {
+                      _profilePic = _downloadProfilerPicture();
+                    });
+                  },
+                  icon: const Icon(Icons.camera_alt),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
